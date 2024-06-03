a624ad3cad1acaac0ad8f61238d4451b5077f047
Cualone <june568@naver.com>
2024-06-03 19:37:04 +0900

    225 modify

--- src/Q023/Main.java
+++ src/Q023/Main.java
@@ -6,6 +6,13 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
+        MyStack stack = new MyStack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.top());
+        System.out.println(stack.pop());
+        System.out.println(stack.empty());
 
     }
 
@@ -14,8 +21,10 @@ public class Main {
 
         public void push(int x) {
             queue.add(x);
-            for (int i = 1;i < queue.size();i++) {}
-            queue.add(queue.remove());
+            for (int i = 1;i < queue.size();i++) {
+                queue.add(queue.remove());
+            }
+
         }
 
         public int pop() {
